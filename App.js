23dc0e23998dfd6d6a851dd385de6276da785ed5
thuyngocha98 <thuyngocha98@gmail.com>
2021-07-23 17:07:10 +0700

    bug: set flag for field

--- App.js
+++ App.js
@@ -10,16 +10,21 @@ import {
   Platform,
   StyleSheet,
   BackHandler,
-  Alert
+  Alert,
+  TouchableOpacity,
+  Text,
 } from 'react-native';
 
-import { DocumentView, RNPdftron } from 'react-native-pdftron';
+import { DocumentView, RNPdftron, Config } from 'react-native-pdftron';
 
 const App = () => {
+  const documentRef = React.useRef(null);
+
   React.useEffect(() => {
     RNPdftron.initialize("Insert commercial license key here after purchase");
     RNPdftron.enableJavaScript(true);
   }, []);
+
   const onLeadingNavButtonPressed = () => {
     console.log('leading nav button pressed');
     if (Platform.OS === 'ios') {
@@ -35,14 +40,39 @@ const App = () => {
       BackHandler.exitApp();
     }
   }
-  const path = "https://pdftron.s3.amazonaws.com/downloads/pl/PDFTRON_mobile_about.pdf";
+
+  const getFieldNames = () => {
+    // All filedName start with "untitled" (I created it);
+    const fieldNames = [...Array(35).keys()].reduce((result, item) => {
+      if (item !== 0 &&item !== 2 && item !== 3) { // fieldName do not exists
+        result.push("untitled" + item);
+      }
+      return result;
+    }, []);
+    return fieldNames;
+  }
+
+  const handleSetFlagForField = () => {
+    const fieldNames = getFieldNames();
+    if(documentRef.current){
+      documentRef.current.setFlagForFields(fieldNames, Config.FieldFlags.ReadOnly, true);
+    }
+  }
+
+  const path = "https://drive.google.com/u/0/uc?id=1idX-xlWJw07CZGHknbx7w-fRZXd32Mhd";
   return (
-    <DocumentView
-      document={path}
-      showLeadingNavButton={true}
-      leadingNavButtonIcon={Platform.OS === 'ios' ? 'ic_close_black_24px.png' : 'ic_arrow_back_white_24dp'}
-      onLeadingNavButtonPressed={onLeadingNavButtonPressed}
-    />
+    <>
+      <DocumentView
+        document={path}
+        ref={documentRef}
+        showLeadingNavButton={true}
+        leadingNavButtonIcon={Platform.OS === 'ios' ? 'ic_close_black_24px.png' : 'ic_arrow_back_white_24dp'}
+        onLeadingNavButtonPressed={onLeadingNavButtonPressed}
+      />
+      <TouchableOpacity onPress={handleSetFlagForField} style={styles.viewBtn}>
+        <Text style={styles.txt}>Set flag for fields</Text>
+      </TouchableOpacity>
+    </>
   );
 };
 
@@ -52,6 +82,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  viewBtn: {
+    margin: 10,
+    padding: 10,
+    backgroundColor: 'gray',
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  txt: {
+    color: 'white',
+    fontSize: 16,
   }
 });
 
