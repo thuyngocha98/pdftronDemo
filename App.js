7a245448339cc21b88f6ce9620a6f6c3dd63a722
thuyngocha98 <thuyngocha98@gmail.com>
2021-08-18 17:11:58 +0700

    bug: text field wrong location

--- App.js
+++ App.js
@@ -5,19 +5,21 @@
  * @format
  * @flow strict-local
  */
-import React from 'react';
+import React, { useState } from "react";
 import {
   Platform,
   StyleSheet,
   BackHandler,
   Alert,
   TouchableOpacity,
+  View,
   Text,
-} from 'react-native';
+} from "react-native";
 
-import { DocumentView, RNPdftron, Config } from 'react-native-pdftron';
+import { DocumentView, RNPdftron, Config } from "react-native-pdftron";
 
 const App = () => {
+  const [showHeader, setShowHeader] = useState(true);
   const documentRef = React.useRef(null);
 
   React.useEffect(() => {
@@ -26,32 +28,52 @@ const App = () => {
   }, []);
 
   const onLeadingNavButtonPressed = () => {
-    console.log('leading nav button pressed');
-    if (Platform.OS === 'ios') {
+    console.log("leading nav button pressed");
+    if (Platform.OS === "ios") {
       Alert.alert(
-        'App',
-        'onLeadingNavButtonPressed',
-        [
-          {text: 'OK', onPress: () => console.log('OK Pressed')},
-        ],
+        "App",
+        "onLeadingNavButtonPressed",
+        [{ text: "OK", onPress: () => console.log("OK Pressed") }],
         { cancelable: true }
-      )
+      );
     } else {
       BackHandler.exitApp();
     }
-  }
+  };
 
-  const path = "https://pdftron.s3.amazonaws.com/downloads/pl/PDFTRON_mobile_about.pdf";
+  const renderCustomHeader = () => {
+    return (
+      <View
+        style={{
+          width: "100%",
+          height: 100,
+          backgroundColor: "green",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>This is custom header!</Text>
+      </View>
+    );
+  };
+
+  // const path = "https://pdftron.s3.amazonaws.com/downloads/pl/PDFTRON_mobile_about.pdf";
+  const path = "http://foersom.com/net/HowTo/data/OoPdfFormExample.pdf"; // form fill
   return (
     <>
+      {showHeader && renderCustomHeader()}
       <DocumentView
         document={path}
         ref={documentRef}
         showLeadingNavButton={true}
-        leadingNavButtonIcon={Platform.OS === 'ios' ? 'ic_close_black_24px.png' : 'ic_arrow_back_white_24dp'}
+        leadingNavButtonIcon={
+          Platform.OS === "ios"
+            ? "ic_close_black_24px.png"
+            : "ic_arrow_back_white_24dp"
+        }
         onLeadingNavButtonPressed={onLeadingNavButtonPressed}
-        onToolChanged = {({previousTool, tool}) => {
-          console.log({ previousTool, tool})
+        onFormFieldValueChanged={({ fields }) => {
+          setShowHeader(false);
         }}
       />
     </>
@@ -61,22 +83,22 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
   viewBtn: {
     margin: 10,
     padding: 10,
-    backgroundColor: 'gray',
+    backgroundColor: "gray",
     borderRadius: 8,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   txt: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
-  }
+  },
 });
 
 export default App;
